a909bc51b468ae63f5dfd011af5a65d4dc6f7347
Darren Hankins <hankins.darren@gmail.com>
2017-01-27 13:54:43 -0700

    public front end

---
+++ public/app.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict'
+
+  angular
+    .module('app', ['ui.router'])
+    .component('dejaVoodoo', {
+      controller: function() {
+        const vm = this;
+
+        vm.$onInit = function() {
+
+      
+        }
+
+
+
+      },
+      templateUrl: './templates/select.template.html'
+    })
+})();
