dffa7925214ffc84d520534ae0c369438e03e666
ZacJocelyn <zacjocelyn@gmail.com>
2017-01-27 13:51:28 -0700

    made routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/noun', (req, res, next) => {
+    return knex('noun')
+    .then(nouns =>{
+      res.json(nouns)
+    })
+})
+router.get('/verb', (req, res, next) => {
+    return knex('verb')
+    .then(verbs =>{
+      res.json(verbs)
+    })
+})
+router.get('/adj', (req, res, next) => {
+    return knex('adj')
+    .then(adjs =>{
+      res.json(adjs)
+    })
+})
+router.get('/meme', (req, res, next) => {
+    return knex('meme')
+    .then(memes =>{
+      res.json(memes)
+    })
+})
 
 module.exports = router;
